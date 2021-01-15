62c100ea20a7b2569a0cf87368f52c9f5d2150d0
tobiaspsp <tobias@powershell.one>
2021-01-15 14:30:51 +0100

    v1

---
+++ Modules/PsOcr/1.0.0/Convert-PsoImageToText.ps1
@@ -0,0 +1,119 @@
+﻿
+class AsyncHelper
+{
+  hidden static [System.Reflection.MethodInfo]$awaiter = $null
+  
+  # initialize the class
+  static AsyncHelper()
+  {    
+    Add-Type -AssemblyName System.Runtime.WindowsRuntime
+    # find the awaiter method
+    [AsyncHelper]::awaiter = [WindowsRuntimeSystemExtensions].GetMember('GetAwaiter', 'Method',  'Public,Static') |
+      Where-Object { $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' } |
+      Select-Object -First 1
+  }
+  
+  # invokes a async method and waits for the return values to be available:
+
+  static [object] Invoke([object]$AsyncTask, [Type]$ResultType)
+  {
+    return [AsyncHelper]::awaiter.
+       MakeGenericMethod($ResultType).
+       Invoke($null, @($AsyncTask)).
+       GetResult()
+  }
+}
+
+function Convert-PsoImageToText
+{
+  <#
+      .SYNOPSIS
+      Converts an image file to text by using Windows 10 built-in OCR
+      .DESCRIPTION
+      Detailed Description
+      .EXAMPLE
+      Convert-ImageToText -Path c:\temp\image.png
+      Converts the image in image.png to text
+      .NOTES
+      Original work from https://github.com/HumanEquivalentUnit/PowerShell-Misc
+
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]
+    [Alias('FullName')]
+    $Path,
+    
+    # dynamically create auto-completion from available OCR languages:
+    [ArgumentCompleter({
+          # receive information about current state:
+          param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    
+          [Windows.Media.Ocr.OcrEngine]::AvailableRecognizerLanguages |
+          Foreach-Object { 
+            # create completionresult items:
+            $displayname = $_.DisplayName
+            $id = $_.LanguageTag
+            [System.Management.Automation.CompletionResult]::new($id, $displayname, "ParameterValue", "$displayName`r`n$id")
+          }
+            })]
+    [Windows.Globalization.Language]
+    $Language
+  )
+  
+  begin
+  { 
+    Add-Type -AssemblyName System.Runtime.WindowsRuntime
+     
+    # [Windows.Media.Ocr.OcrEngine]::AvailableRecognizerLanguages
+    if ($PSBoundParameters.ContainsKey('Language'))
+    {
+      $ocrEngine = [Windows.Media.Ocr.OcrEngine]::TryCreateFromLanguage($Language)
+    }
+    else
+    {
+      $ocrEngine = [Windows.Media.Ocr.OcrEngine]::TryCreateFromUserProfileLanguages()
+    }
+  
+    
+    # PowerShell doesn't have built-in support for Async operations, 
+    # but all the WinRT methods are Async.
+    # This function wraps a way to call those methods, and wait for their results.
+    
+  }
+  
+  process
+  {
+    # all of these methods run asynchronously because they are tailored for responsive UIs
+    # PowerShell is single-threaded and synchronous so a helper class is used to 
+    # run the async methods and wait for them to complete, essentially reversing the async 
+    # behavior
+    
+    # [AsyncHelper]::Invoke() requires the async method and the desired return type
+  
+    # get image file:
+    $file = [Windows.Storage.StorageFile]::GetFileFromPathAsync($path)
+    $storageFile = [AsyncHelper]::Invoke($file, [Windows.Storage.StorageFile])
+  
+    # read image content:
+    $content = $storageFile.OpenAsync([Windows.Storage.FileAccessMode]::Read)
+    $fileStream = [AsyncHelper]::Invoke($content, [Windows.Storage.Streams.IRandomAccessStream])
+  
+    # get bitmap decoder:
+    $decoder = [Windows.Graphics.Imaging.BitmapDecoder]::CreateAsync($fileStream)
+    $bitmapDecoder = [AsyncHelper]::Invoke($decoder, [Windows.Graphics.Imaging.BitmapDecoder])
+  
+    # decode bitmap:
+    $bitmap = $bitmapDecoder.GetSoftwareBitmapAsync()
+    $softwareBitmap = [AsyncHelper]::Invoke($bitmap, [Windows.Graphics.Imaging.SoftwareBitmap])
+  
+    # do optical text recognition (OCR) and return lines and words:
+    $ocrResult = $ocrEngine.RecognizeAsync($softwareBitmap)
+    [AsyncHelper]::Invoke($ocrResult, [Windows.Media.Ocr.OcrResult]).Lines | 
+      Select-Object -Property Text, @{Name='Words';Expression={$_.Words.Text}}
+  }
+}
+
+Set-Alias -Name Convert-ImageToText -Value Convert-PsoImageToText
---
+++ Modules/PsOcr/1.0.0/PsOcr.psd1
@@ -0,0 +1,70 @@
+﻿
+
+@{
+
+# Module Loader File
+RootModule = 'root.ps1'
+
+# Version Number
+ModuleVersion = '1.0.0'
+
+# Unique Module ID
+GUID = '8b8b571f-57e4-419b-97fe-ac61a5aaed34'
+
+# Module Author
+Author = 'Dr. Tobias Weltner'
+
+# Company
+CompanyName = 'https://powershell.one'
+
+# Copyright
+Copyright = '(c) 2021 Dr. Tobias Weltner. All rights reserved.'
+
+# Module Description
+Description = 'convert images to text by using the Windows 10 built-in OCR engine'
+
+# Minimum PowerShell Version Required
+PowerShellVersion = '5.1'
+CompatiblePSEditions = @('Desktop')
+
+# Name of Required PowerShell Host
+PowerShellHostName = ''
+
+# Minimum Host Version Required
+PowerShellHostVersion = ''
+
+# List of exportable functions
+FunctionsToExport = 'Convert-PsoImageToText'
+
+
+# List of exportable aliases
+AliasesToExport = 'Convert-ImageToText'
+
+# Private data that needs to be passed to this module
+PrivateData          = @{
+
+        PSData = @{
+
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags       = @(
+                'OCR'
+                'WinRT'
+                'Async'
+                'Class'
+                'Await'
+                'powershell.one'
+                'Windows'
+            )
+
+            # A URL to the license for this module.
+            #LicenseUri = 'https://github.com/xxx/blob/master/LICENSE'
+
+            # A URL to the main website for this project.
+            ProjectUri = 'https://github.com/TobiasPSP/PsOcr'
+
+
+        } # End of PSData hashtable
+
+    } # End of PrivateData hashtable
+
+}
